fd049cbd850483978cca19cf6124dd60b9d8799a
Sheila Ngetich <sanaipey@gmail.com>
2019-02-02 22:33:32 -0600

    created Hello world

---
+++ src/main/java/org/launchcode/myrecipe/MyRecipeApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.myrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipeApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/launchcode/myrecipe/MyRecipeApplicationTests.java
@@ -0,0 +1,17 @@
+package org.launchcode.myrecipe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRecipeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
