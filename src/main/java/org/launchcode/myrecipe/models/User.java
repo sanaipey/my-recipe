c740f4fb24d4d6c10c92da335b8682df91903ac1
Sheila Ngetich <sanaipey@gmail.com>
2019-02-13 20:16:12 -0600

    created userId field/property and default constructor for it

--- src/main/java/org/launchcode/myrecipe/models/User.java
+++ src/main/java/org/launchcode/myrecipe/models/User.java
@@ -5,15 +5,29 @@ public class User {
 
 
     private String userName;
-
     private String email;
-
     private String password;
+    private int userId;
+    private static int nextId = 1;  //initializes userId so that it's unique for every single user object
+
+
 
     public User() {
+        userId = nextId;
+        nextId++;
     }
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
     public User(String userName, String email, String password){
+        this(); //calls default constructor for the User class and initializes Id field
         this.userName = userName;
         this.email = email;
         this.password = password;
